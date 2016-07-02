a0d5af1f1dad32ec376c150bc0da7b2e74ce5b32
Francois W. Nel <francoiswnel@gmail.com>
2016-07-02 11:19:27 +0200

    Rewrote the code translator

--- CodeTranslator.cpp
+++ CodeTranslator.cpp
@@ -6,142 +6,85 @@
 
 using namespace std;
 
-string CodeTranslator::dest(string destMnemonic) {
-    if (destMnemonic.compare("M") == 0) {
-        return "001";
+CodeTranslator::CodeTranslator() {
+    // Populate the translation map tables.
+    destTable[""] = "000";
+    destTable["M"] = "001";
+    destTable["D"] = "010";
+    destTable["MD"] = "011";
+    destTable["A"] = "100";
+    destTable["AM"] = "101";
+    destTable["AD"] = "110";
+    destTable["AMD"] = "111";
+
+    compTable["0"] = "0101010";
+    compTable["1"] = "0111111";
+    compTable["-1"] = "0111010";
+    compTable["D"] = "0001100";
+    compTable["A"] = "0110000";
+    compTable["!D"] = "0001101";
+    compTable["!A"] = "0110001";
+    compTable["-D"] = "0001111";
+    compTable["-A"] = "0110011";
+    compTable["D+1"] = "0011111";
+    compTable["A+1"] = "0110111";
+    compTable["D-1"] = "0001110";
+    compTable["A-1"] = "0110010";
+    compTable["D+A"] = "0000010";
+    compTable["D-A"] = "0010011";
+    compTable["A-D"] = "0000111";
+    compTable["D&A"] = "0000000";
+    compTable["D|A"] = "0010101";
+    compTable["M"] = "1110000";
+    compTable["!M"] = "1110001";
+    compTable["-M"] = "1110011";
+    compTable["M+1"] = "1110111";
+    compTable["M-1"] = "1110010";
+    compTable["D+M"] = "1000010";
+    compTable["D-M"] = "1010011";
+    compTable["M-D"] = "1000111";
+    compTable["D&M"] = "1000000";
+    compTable["D|M"] = "1010101";
+
+    jumpTable[""] = "000";
+    jumpTable["JGT"] = "001";
+    jumpTable["JEQ"] = "010";
+    jumpTable["JGE"] = "011";
+    jumpTable["JLT"] = "100";
+    jumpTable["JNE"] = "101";
+    jumpTable["JLE"] = "110";
+    jumpTable["JMP"] = "111";
+}
+
+string CodeTranslator::dest(string destMnemonic, unsigned long& lineNr) {
+    if (destTable.find(destMnemonic) != destTable.end()) {
+        return destTable[destMnemonic];
     }
-    else if (destMnemonic.compare("D") == 0) {
-        return "010";
-    }
-    else if (destMnemonic.compare("MD") == 0) {
-        return "011";
-    }
-    else if (destMnemonic.compare("A") == 0) {
-        return "100";
-    }
-    else if (destMnemonic.compare("AM") == 0) {
-        return "101";
-    }
-    else if (destMnemonic.compare("AD") == 0) {
-        return "110";
-    }
-    else if (destMnemonic.compare("AMD") == 0) {
-        return "111";
-    }
-    return "000";
+
+    // If none of the mnemonics are found, output an error message,
+    //  and provide the line number in the original source where the error occurred.
+    cout << "Invalid syntax in destination statement at line: " << lineNr << endl;
+    exit(1);
 }
 
 string CodeTranslator::comp(string compMnemonic, unsigned long& lineNr) {
-    if (compMnemonic.compare("0") == 0) {
-        return "0101010";
-    }
-    else if (compMnemonic.compare("1") == 0) {
-        return "0111111";
-    }
-    else if (compMnemonic.compare("-1") == 0) {
-        return "0111010";
-    }
-    else if (compMnemonic.compare("D") == 0) {
-        return "0001100";
-    }
-    else if (compMnemonic.compare("A") == 0) {
-        return "0110000";
-    }
-    else if (compMnemonic.compare("!D") == 0) {
-        return "0001101";
-    }
-    else if (compMnemonic.compare("!A") == 0) {
-        return "0110001";
-    }
-    else if (compMnemonic.compare("-D") == 0) {
-        return "0001111";
-    }
-    else if (compMnemonic.compare("-A") == 0) {
-        return "0110011";
-    }
-    else if (compMnemonic.compare("D+1") == 0) {
-        return "0011111";
-    }
-    else if (compMnemonic.compare("A+1") == 0) {
-        return "0110111";
-    }
-    else if (compMnemonic.compare("D-1") == 0) {
-        return "0001110";
-    }
-    else if (compMnemonic.compare("A-1") == 0) {
-        return "0110010";
-    }
-    else if (compMnemonic.compare("D+A") == 0) {
-        return "0000010";
-    }
-    else if (compMnemonic.compare("D-A") == 0) {
-        return "0010011";
-    }
-    else if (compMnemonic.compare("A-D") == 0) {
-        return "0000111";
-    }
-    else if (compMnemonic.compare("D&A") == 0) {
-        return "0000000";
-    }
-    else if (compMnemonic.compare("D|A") == 0) {
-        return "0010101";
-    }
-    else if (compMnemonic.compare("M") == 0) {
-        return "1110000";
-    }
-    else if (compMnemonic.compare("!M") == 0) {
-        return "1110001";
-    }
-    else if (compMnemonic.compare("-M") == 0) {
-        return "1110011";
-    }
-    else if (compMnemonic.compare("M+1") == 0) {
-        return "1110111";
-    }
-    else if (compMnemonic.compare("M-1") == 0) {
-        return "1110010";
-    }
-    else if (compMnemonic.compare("D+M") == 0) {
-        return "1000010";
-    }
-    else if (compMnemonic.compare("D-M") == 0) {
-        return "1010011";
-    }
-    else if (compMnemonic.compare("M-D") == 0) {
-        return "1000111";
-    }
-    else if (compMnemonic.compare("D&M") == 0) {
-        return "1000000";
-    }
-    else if (compMnemonic.compare("D|M") == 0) {
-        return "1010101";
+    if (compTable.find(compMnemonic) != compTable.end()) {
+        return compTable[compMnemonic];
     }
 
+    // If none of the mnemonics are found, output an error message,
+    //  and provide the line number in the original source where the error occurred.
     cout << "Invalid syntax in computation statement at line: " << lineNr << endl;
     exit(1);
 }
 
-string CodeTranslator::jump(string jumpMnemonic) {
-    if (jumpMnemonic.compare("JGT") == 0) {
-        return "001";
+string CodeTranslator::jump(string jumpMnemonic, unsigned long& lineNr) {
+    if (jumpTable.find(jumpMnemonic) != jumpTable.end()) {
+        return jumpTable[jumpMnemonic];
     }
-    else if (jumpMnemonic.compare("JEQ") == 0) {
-        return "010";
-    }
-    else if (jumpMnemonic.compare("JGE") == 0) {
-        return "011";
-    }
-    else if (jumpMnemonic.compare("JLT") == 0) {
-        return "100";
-    }
-    else if (jumpMnemonic.compare("JNE") == 0) {
-        return "101";
-    }
-    else if (jumpMnemonic.compare("JLE") == 0) {
-        return "110";
-    }
-    else if (jumpMnemonic.compare("JMP") == 0) {
-        return "111";
-    }
-    return "000";
+
+    // If none of the mnemonics are found, output an error message,
+    //  and provide the line number in the original source where the error occurred.
+    cout << "Invalid syntax in jump statement at line: " << lineNr << endl;
+    exit(1);
 }
--- CodeTranslator.h
+++ CodeTranslator.h
@@ -6,23 +6,34 @@
 #define HACK_ASSEMBLER_CODE_H
 
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 class CodeTranslator {
 public:
-    string dest(string destMnemonic);
+    CodeTranslator();
+    // Populates the code translation map tables
+    //  with the language specification.
+
+    string dest(string destMnemonic, unsigned long& lineNr);
     // Returns the binary code of the destination mnemonic
-    // as a string containing 3 bits.
+    //  as a string containing 3 bits.
+    // Line number is passed for use in an error message.
 
     string comp(string compMnemonic, unsigned long& lineNr);
     // Returns the binary code of the computation mnemonic
-    // as a string containing 7 bits.
+    //  as a string containing 7 bits.
     // Line number is passed for use in an error message.
 
-    string jump(string jumpMnemonic);
+    string jump(string jumpMnemonic, unsigned long& lineNr);
     // Returns the binary code of the jump mnemonic
-    // as a string containing 3 bits.
+    //  as a string containing 3 bits.
+    // Line number is passed for use in an error message.
+private:
+    map<string, string> destTable;
+    map<string, string> compTable;
+    map<string, string> jumpTable;
 };
 
 #endif //HACK_ASSEMBLER_CODE_H
